2886ab10d1db7b803294c1dd5023d1ac77049071
Rohit Pandey <rpandey1627@gmail.com>
2026-03-17 06:18:15 +0000

    Day 4

---
+++ 01_Basics/07_Dates_Time.js
@@ -0,0 +1,7 @@
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(typeof myDate)
+
+let myCreatedDate = new Date(2023,0,23)console.log(myCreatedDate)
+let myCreatedDate = new Date(2023,0,23)console.log(myCreatedDate.toString())

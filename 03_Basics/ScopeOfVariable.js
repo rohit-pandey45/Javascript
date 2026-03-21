a0dd38e0d6c46c8e2d61c0c0d6327013ef4f7083
Rohit Pandey <rpandey1627@gmail.com>
2026-03-21 08:26:02 +0000

    Day 7 : Scope of Variables

---
+++ 03_Basics/ScopeOfVariable.js
@@ -0,0 +1,18 @@
+let a = 300
+if(true){
+    let a = 10
+    const b = 20
+    console.log("INNER :", a)
+}
+// console.log(a)
+
+function one(){
+    const username = "hitesh"
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+    console.log(website)
+    two()
+}
+one()

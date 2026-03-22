125e5b07dca92939ce37314928587decab59b752
Rohit Pandey <rpandey1627@gmail.com>
2026-03-22 11:33:48 +0000

    For Loop with Break and Continue

---
+++ 06_Iteration/one.js
@@ -0,0 +1,26 @@
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     console.log(element)
+    
+// } 
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`Outer Loop Value : ${i}`);
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`Inner Loop Value : ${j}`);
+        
+//     }
+    
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`Outer Loop Value : ${i}`);
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`Inner Loop Value : ${j}`);
+//         console.log(i + '*' + j + ' = ' + i*j);
+//     }
+    
+// }
+
+//BREAK and CONTINUE
+
